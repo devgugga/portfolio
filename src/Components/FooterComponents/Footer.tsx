fc22cb10290fc772d9b847b381aaa3931ce6697d
devgugga <gustavogomes90104@gmail.com>
2024-12-17 16:35:22 -0300

    💄implementa componente DesktopFooter (main)
    Implementa o componente DesktopFooter, estilizando-o e
    adicionando links para as redes sociais. Inclui ícones
    para cada rede social e ajusta o layout para telas
    maiores.

--- src/Components/FooterComponents/Footer.tsx
+++ src/Components/FooterComponents/Footer.tsx
@@ -2,13 +2,14 @@ import * as S from "./_style.ts";
 import { ApplicationContext } from "../../Context/Contexts.ts";
 import { useContext } from "react";
 import { MobileFooter } from "./Mobile/MobileFooter.tsx";
+import { DesktopFooter } from "./Desktop/DesktopFooter.tsx";
 
 export const Footer = () => {
   const { mobile } = useContext(ApplicationContext);
 
   return (
     <S.FooterContainer>
-      {mobile ? <MobileFooter /> : "Footer"}
+      {mobile ? <MobileFooter /> : <DesktopFooter />}
     </S.FooterContainer>
   );
 };
--- src/Components/FooterComponents/_style.ts
+++ src/Components/FooterComponents/_style.ts
@@ -26,3 +26,37 @@ export const MobileFooterContent = styled.div`
     border-left: 1px solid ${({ theme }) => theme.colors.border};
   }
 `;
+
+// Desktop Styles
+
+export const DesktopFooterContent = styled.div`
+  display: grid;
+  grid-template-columns: auto auto auto 1fr;
+
+  p {
+    padding: 0 18px;
+
+    color: ${({ theme }) => theme.colors.text.default};
+
+    align-self: center;
+  }
+
+  .icons {
+    padding: 12px 15px;
+
+    border-left: 1px solid ${({ theme }) => theme.colors.border};
+  }
+
+  .instaIcon {
+    border-right: 1px solid ${({ theme }) => theme.colors.border};
+  }
+
+  .lastIcon {
+    display: flex;
+    align-items: center;
+    justify-self: right;
+    gap: 5px;
+
+    color: ${({ theme }) => theme.colors.text.default};
+  }
+`;
